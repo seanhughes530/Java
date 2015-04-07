2251d6b26a66c776b4d451b0be8d089d83e65888
Sean Hughes <seanhughes530@gmail.com>
2015-04-06 22:07:23 -0400

    updates

--- Game/js/index.js
+++ Game/js/index.js
@@ -1,6 +1,46 @@
-$(document).ready(function(){
-      $('div.bar-1').mousemove(function(e){
-            var y = e.pageY;
-            $('.moveAble').css('top', y-20);
-      });
-    });
+var square1 = $('.square-1');
+var square2 = $('.square-2');
+var square3 = $('.square-3');
+var square4 = $('.square-4');
+var square5 = $('.square-5');
+var square6 = $('.square-6');
+var square7 = $('.square-7');
+var square8 = $('.square-8');
+var square9 = $('.square-9');
+var symbol1 = $('.square-1 h3');
+var symbol2 = $('.square-2 h3');
+var symbol3 = $('.square-3 h3');
+var symbol4 = $('.square-4 h3');
+var symbol5 = $('.square-5 h3');
+var symbol6 = $('.square-6 h3');
+var symbol7 = $('.square-7 h3');
+var symbol8 = $('.square-8 h3');
+var symbol9 = $('.square-9 h3');
+
+square1.click (function (e){
+	symbol1.text("X");
+});
+square2.click (function (e){
+	symbol2.text("X");
+});
+square3.click (function (e){
+	symbol3.text("X");
+});
+square4.click (function (e){
+	symbol4.text("X");
+});
+square5.click (function (e){
+	symbol5.text("X");
+});
+square6.click (function (e){
+	symbol6.text("X");
+});
+square7.click (function (e){
+	symbol7.text("X");
+});
+square8.click (function (e){
+	symbol8.text("X");
+});
+square9.click (function (e){
+	symbol9.text("X");
+});
